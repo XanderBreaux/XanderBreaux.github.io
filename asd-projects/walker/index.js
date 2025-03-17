7c41b195c434cc1ad78c4e888f5ea669ad169418
XanderBreaux <141953829+XanderBreaux@users.noreply.github.com>
2025-03-17 16:33:35 +0000

    added tag to Walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -28,6 +28,7 @@ function runProgram(){
     speedX: 0,
     speedY: 0,
     ID: "#walker1",
+    isIt: true
   }
   var walker2 = {
     positionX: $("#board").width() - 50,
@@ -35,12 +36,14 @@ function runProgram(){
     speedX: 0,
     speedY: 0,
     ID: "#walker2",
+    isIt: false
   }
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
   $(document).on('keyup', handleKeyUp);
+  $(".gameWalker").on('click', changeColor);
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -54,11 +57,16 @@ function runProgram(){
     repositionGameItem(walker1);
     wallCollisions(walker1);
     redrawGameItem(walker1);
+    isSeeker(walker1);
 
     /// For Walker2
     repositionGameItem(walker2);
     wallCollisions(walker2);
     redrawGameItem(walker2);
+    isSeeker(walker2);
+
+    handleCollision();
+
   }
   
   /* 
@@ -67,29 +75,62 @@ function runProgram(){
   function handleKeyDown(event) {
     if (event.which === KEY.LEFT) {
       console.log("Left Pressed");
-      walker1.speedX = -5;
+      if (walker1.isIt === false) {
+        walker1.speedX = -5;
+      } else {
+        walker1.speedX = -3;
+      }
     } else if ( event.which === KEY.UP) {
       console.log("Up Pressed");
-      walker1.speedY = -5;
+      if (walker1.isIt === false) {
+        walker1.speedY = -5;
+      } else {
+        walker1.speedY = -3;
+      }
     } else if ( event.which === KEY.RIGHT) {
       console.log("Right Pressed");
-      walker1.speedX = 5;
+      if (walker1.isIt === false) {
+        walker1.speedX = 5;
+      } else {
+        walker1.speedX = 3;
+      }
     } else if ( event.which === KEY.DOWN) {
       console.log("Down Pressed");
-      walker1.speedY = 5;
+      if (walker1.isIt === false) {
+        walker1.speedY = 5;
+      } else {
+        walker1.speedY = 3;
+      }
     }
     if (event.which === KEY.A) {
       console.log("Left Pressed");
-      walker2.speedX = -5;
+      if (walker2.isIt === false) {
+        walker2.speedX = -5;
+      } else {
+        walker2.speedX = -3;
+      }
     } else if ( event.which === KEY.W) {
       console.log("Up Pressed");
-      walker2.speedY = -5;
+      if (walker2.isIt === false) {
+        walker2.speedY = -5;
+      } else {
+        walker2.speedY = -3;
+      }
     } else if ( event.which === KEY.D) {
       console.log("Right Pressed");
-      walker2.speedX = 5;
+      if (walker2.isIt === false) {
+        walker2.speedX = 5;
+      } else {
+        walker2.speedX = 3;
+      }
     } else if ( event.which === KEY.S) {
       console.log("Down Pressed");
-      walker2.speedY = 5;
+      if (walker2.isIt === false) {
+        walker2.speedY = 5;
+      } else {
+        walker2.speedY = 3;
+      }
+  
     }
   }
 
@@ -144,6 +185,42 @@ function runProgram(){
       gameItem.positionY = $("#board").height() - 50;
     } else if (gameItem.positionY < 0) {
       gameItem.positionY = 0;
+    }
+  }
+
+  /// handle game item collisions
+  function handleCollision() {
+    if (walker1.positionX > walker2.positionX && walker1.positionX < walker2.positionX + 50) {
+      if (walker1.positionY > walker2.positionY && walker1.positionY < walker2.positionY + 50) {
+        if (walker1.isIt) {
+          walker1.isIt = false;
+          walker2.isIt = true;
+        } else {
+          walker1.isIt = true;
+          walker2.isIt = false;
+        }
+        walker1.positionX = 0;
+        walker1.positionY = 0;
+        walker2.positionX = $("#board").width() - 50;
+        walker2.positionY = 0;
+      }
+    }
+  }
+
+  /// Gives game item a random color
+  function changeColor() {
+    var randomColor = "#000000".replace(/0/g, function () {
+      return (~~(Math.random() * 16)).toString(16);
+    });
+    $(event.currentTarget).css("background-color", randomColor);
+  }
+
+  /// Gives the seeker a lower border radius
+  function isSeeker(gameItem) {
+    if (gameItem.isIt === true) {
+      $(gameItem.ID).css("border-radius", 15);
+    } else {
+      $(gameItem.ID).css("border-radius", 50);
     }
   }
 }
